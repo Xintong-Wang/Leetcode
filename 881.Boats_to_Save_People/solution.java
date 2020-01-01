37a45d064df4c2e4d063dcd0866499467ad0e5eb
Xintong-Wang <xintongwang0710@gmail.com>
2020-01-01 13:19:50 -0800

    Create solution.java

---
+++ 881.Boats_to_Save_People/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int res = 0;
+        int i = 0, j = people.length - 1;
+        while(i <= j){
+            if(people[i] + people[j] <= limit){
+                i++;
+                j--;
+                res++;
+            }
+            else{
+                j--;
+                res++;
+            }
+        }
+        return res;  
+    }
+}
