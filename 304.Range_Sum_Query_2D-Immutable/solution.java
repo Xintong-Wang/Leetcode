e9c7c92e08b0985065a644ecb4e276bfa50a1bf9
Xintong-Wang <xintongwang0710@gmail.com>
2020-02-04 00:11:02 -0800

    Update solution.java

--- 304.Range_Sum_Query_2D-Immutable/solution.java
+++ 304.Range_Sum_Query_2D-Immutable/solution.java
@@ -5,7 +5,7 @@ class NumMatrix {
         if(matrix.length == 0 || matrix[0].length == 0)
             return;
         int m = matrix.length, n = matrix[0].length;
-        int[][] dp= new int[m + 1][n + 1];
+        dp= new int[m + 1][n + 1];
         for(int i = 1; i <= m; i++){
             for(int j = 1; j <= n; j++){
                 dp[i][j] = matrix[i - 1][j - 1]
