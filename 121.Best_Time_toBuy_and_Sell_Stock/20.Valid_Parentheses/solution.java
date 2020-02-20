9465d4602d98684177cc6668a5e4f602aacf8cf1
Xintong-Wang <xintongwang0710@gmail.com>
2020-02-19 17:29:05 -0800

    Create solution.java

---
+++ 121.Best_Time_toBuy_and_Sell_Stock/20.Valid_Parentheses/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        for(int i = 0; i < s.length(); i++){
+            if(stack.empty())
+                stack.push(s.charAt(i));
+            else if((stack.peek() == '(' && s.charAt(i) == ')') || (stack.peek() == '[' && s.charAt(i) == ']') || (stack.peek() == '{' && s.charAt(i) == '}'))
+                stack.pop();
+            else
+                stack.push(s.charAt(i));
+        }
+        return(stack.empty());
+    }
+}
