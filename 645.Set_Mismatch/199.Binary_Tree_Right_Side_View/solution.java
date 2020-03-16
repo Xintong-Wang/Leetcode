578a7e4a643db1880f0097d4d631feb59c6f26b6
Xintong-Wang <xintongwang0710@gmail.com>
2020-03-15 21:49:16 -0700

    Update solution.java

--- 645.Set_Mismatch/199.Binary_Tree_Right_Side_View/solution.java
+++ 645.Set_Mismatch/199.Binary_Tree_Right_Side_View/solution.java
@@ -13,15 +13,15 @@ class Solution {
         if(root == null)
             return res;
         Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root);
+        queue.offer(root);
         while(!queue.isEmpty()){
             int n = queue.size();
             while(n > 0){
                 root = queue.poll();
                 if(root.left != null)
-                    queue.add(root.left);
+                    queue.offer(root.left);
                 if(root.right != null)
-                    queue.add(root.right);
+                    queue.offer(root.right);
                 n--;
                 if(n == 0)
                     res.add(root.val);
